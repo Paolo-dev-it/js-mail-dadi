d5f5654208939a77e4043db9d64a557355651218
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-24 16:13:52 +0200

    Working on variables js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
-let nome = "paolo";
-console.log(nome);
+let eMail = document.getElementById("eMail");
+
+
+let emailTrue = true;
+
+
+function registerMail(){
+    const mail = eMail.value   //Email che inserirà l'utente
+
+    let mailRegistered = ["michele.n@virgilio.it", "paolo.nicoletti99@gmail.com", "carde.elisa@gmail.com"];
+
+    for (i = 0; i < mailRegistered.length; i++ ) {
+
+        if ( mail === mailRegistered[i] ){
+            console.log("benvenuto");
+            emailTrue = false;
+        }
+         if ( mail != mailRegistered[i] ){
+            console.log("registrati");
+            emailTrue = true;
+        }
+            
+        
+    }
+}
